a745642a2aaaadf2e25c6a7f3a478c842cb98250
Joe Joyce <joe@decafjoe.com>
2017-07-25 12:44:48 -0400

    Updates dependencies to their latest versions.
    * sqlalchemy (1.1.11 -> 1.1.12)

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ requires = (
     'clik==0.5-alpha.2',
     'pexpect==4.2.1',
     'python-dateutil==2.6.1',
-    'sqlalchemy==1.1.11',
+    'sqlalchemy==1.1.12',
     'sqlalchemy-utils==0.32.14',
 )
 
