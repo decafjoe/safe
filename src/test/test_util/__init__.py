b1ff56cba1cd25cc904dae919327cc38794c717f
Joe Joyce <joe@decafjoe.com>
2017-11-13 02:20:46 -0500

    Adds tests for safe.util.

---
+++ src/test/test_util/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Tests for :mod:`safe.util`.
+
+:author: Joe Joyce <joe@decafjoe.com>
+:copyright: Copyright (c) Joe Joyce and contributors, 2016-2017.
+:license: BSD
+"""
---
+++ src/test/test_util/test_prompt_bool.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Tests for :func:`safe.util.prompt_bool`.
+
+:author: Joe Joyce <joe@decafjoe.com>
+:copyright: Copyright (c) Joe Joyce and contributors, 2016-2017.
+:license: BSD
+"""
+import mock
+import pytest
+
+from safe.util import prompt_bool
+
+
+@pytest.mark.parametrize('default,expected,values', (
+    (False, False, ('', 'n', 'N', 'foo', 'asdf')),
+    (False, True, ('y', 'Y')),
+    (True, False, ('n', 'N')),
+    (True, True, ('', 'y', 'Y', 'foo', 'asdf')),
+))
+def test(default, expected, values):
+    for value in values:
+        with mock.patch('safe.util.input') as input:
+            input.return_value = value
+            assert expected == prompt_bool('', default)
