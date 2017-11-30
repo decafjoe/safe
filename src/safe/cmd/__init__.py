8fd5e5668668c573e63486d97dc0f374f7730998
Joe Joyce <joe@decafjoe.com>
2017-11-30 14:45:11 -0500

    Adds command for generating secrets from policy.

--- src/safe/cmd/__init__.py
+++ src/safe/cmd/__init__.py
@@ -6,7 +6,8 @@ Subpackage containing the commands for the application.
 :copyright: Copyright (c) Joe Joyce and contributors, 2016-2017.
 :license: BSD
 """
-import safe.cmd.gen
+import safe.cmd.gen.bare
+import safe.cmd.gen.per_policy
 import safe.cmd.init
 import safe.cmd.ipy
 import safe.cmd.new.account
