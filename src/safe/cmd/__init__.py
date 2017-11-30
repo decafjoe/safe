73b5f3bd94a7e7c7f0783eb5cde545f3f6c11864
Joe Joyce <joe@decafjoe.com>
2017-11-29 22:45:39 -0500

    New command set, with fig leaf sanity tests.

--- src/safe/cmd/__init__.py
+++ src/safe/cmd/__init__.py
@@ -6,3 +6,9 @@ Subpackage containing the commands for the application.
 :copyright: Copyright (c) Joe Joyce and contributors, 2016-2017.
 :license: BSD
 """
+import safe.cmd.gen
+import safe.cmd.init
+import safe.cmd.ipy
+import safe.cmd.new.account
+import safe.cmd.shell
+import safe.cmd.update.account
---
+++ src/safe/cmd/update/account.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Update account command.
+
+:author: Joe Joyce <joe@decafjoe.com>
+:copyright: Copyright (c) Joe Joyce and contributors, 2016-2017.
+:license: BSD
+"""
+import sys
+
+from clik import args, parser
+
+from safe.cmd.update import update
+from safe.ec import UNRECOGNIZED_ACCOUNT, VALIDATION_ERROR
+from safe.form import UpdateAccountForm
+from safe.model import Account
+
+
+@update
+def account():
+    """Update an account and/or its associated data."""
+    parser.add_argument(
+        'account',
+        help='name or alias of account to update',
+        nargs=1,
+    )
+    parser.add_argument(
+        '-p',
+        '--password',
+        action='store_true',
+        default=False,
+        help='set the password for an account (prompts for value)',
+    )
+
+    form = UpdateAccountForm()
+    form.configure_parser()
+
+    yield
+
+    account = Account.for_slug(args.account[0])
+    if account is None:
+        print('error: no account with name/alias:', args.account)
+        yield UNRECOGNIZED_ACCOUNT
+
+    if not form.bind_and_validate(account):
+        msg = 'error: there were validation error(s) with input value(s)'
+        print(msg, file=sys.stderr)
+        form.print_errors()
+        yield VALIDATION_ERROR
+
+    # TODO(jjoyce): prompt for password if -p/--password was supplied
+    # TODO(jjoyce): look at question operations and prompt for new and
+    #               updated values
+
+    form.update_account()
+    g.commit_and_save()
