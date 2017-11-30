04ce8a586dbd612e51f165cf46f723fe01c17513
Joe Joyce <joe@decafjoe.com>
2017-11-29 22:29:47 -0500

    Updates clik from 0.90.2 to 0.92.1.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools import find_packages, setup
 name = 'decafjoe-safe'
 version = '0.6.0'
 requires = (
-    'clik==0.90.2',
+    'clik==0.92.1',
     'clik-shell==0.90.0',
     'clik-wtforms==0.90.1',
     'sqlalchemy==1.1.14',
